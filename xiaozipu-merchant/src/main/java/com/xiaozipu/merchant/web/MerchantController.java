be7a4fc4f09c8d6b8ab17b51ae5c60aa5ac0707a
殷俊杰 <247385709@qq.com>
2020-03-04 22:11:00 +0800

    商户

---
+++ xiaozipu-merchant/src/main/java/com/xiaozipu/merchant/web/MerchantController.java
@@ -0,0 +1,12 @@
+package com.xiaozipu.merchant.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: YinJunJie
+ * @date: 2020/3/4 22:10
+ * @description:
+ */
+@RestController
+public class MerchantController {
+}
