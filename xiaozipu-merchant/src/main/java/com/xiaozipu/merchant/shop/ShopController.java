5e838fee24568c5f06b7e693792ae4b17e731a11
殷俊杰 <247385709@qq.com>
2020-03-09 17:21:53 +0800

    商家订单

---
+++ xiaozipu-merchant/src/main/java/com/xiaozipu/merchant/shop/ShopController.java
@@ -0,0 +1,9 @@
+package com.xiaozipu.merchant.shop;
+
+/**
+ * @author: YinJunJie
+ * @date: 2020/3/3 15:29
+ * @description:
+ */
+public class ShopController {
+}
---
+++ xiaozipu-merchant/src/main/java/com/xiaozipu/merchant/web/OrderController.java
@@ -0,0 +1,16 @@
+package com.xiaozipu.merchant.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: YinJunJie
+ * @date: 2020/3/3 14:16
+ * @description:
+ */
+@RestController
+public class OrderController {
+    private static final Logger logger= LoggerFactory.getLogger(OrderController.class);
+
+}
