e72feee27b7b4c17cb34157318d3331870d037a1
殷俊杰 <247385709@qq.com>
2020-03-25 11:06:53 +0800

    打印日志

--- xiaozipu-third/src/main/java/com/xiaozipu/third/util/mp/MpUtils.java
+++ xiaozipu-third/src/main/java/com/xiaozipu/third/util/mp/MpUtils.java
@@ -2,6 +2,8 @@ package com.xiaozipu.third.util.mp;
 
 import net.sf.json.JSONObject;
 import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author: YinJunJie
@@ -9,30 +11,32 @@ import org.apache.commons.codec.binary.Base64;
  * @description:
  */
 public class MpUtils {
-
+    private static final Logger logger = LoggerFactory.getLogger(MpUtils.class);
     private static final String WATERMARK = "watermark";
     private static final String APPID = "appid";
+
     /**
      * 解密数据
+     *
      * @return
      * @throws Exception
      */
-    public static String decrypt(String appId, String encryptedData, String sessionKey, String iv){
+    public static String decrypt(String appId, String encryptedData, String sessionKey, String iv) {
         String result = "";
         try {
             AES aes = new AES();
             byte[] resultByte = aes.decrypt(Base64.decodeBase64(encryptedData), Base64.decodeBase64(sessionKey), Base64.decodeBase64(iv));
-            if(null != resultByte && resultByte.length > 0){
+            if (null != resultByte && resultByte.length > 0) {
                 result = new String(WxPKCS7Encoder.decode(resultByte));
                 JSONObject jsonObject = JSONObject.fromObject(result);
                 String decryptAppid = jsonObject.getJSONObject(WATERMARK).getString(APPID);
-                if(!appId.equals(decryptAppid)){
+                if (!appId.equals(decryptAppid)) {
                     result = "";
                 }
             }
         } catch (Exception e) {
             result = "";
-            e.printStackTrace();
+            logger.info("解密数据异常:", e);
         }
         return result;
     }
