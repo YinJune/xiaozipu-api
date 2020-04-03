9d3080f6ba2dc8e4f0d87c161ed92752ff17795b
殷俊杰 <247385709@qq.com>
2020-04-03 23:34:25 +0800

    dingd

--- xiaozipu-merchant/src/main/java/com/xiaozipu/merchant/web/OrderController.java
+++ xiaozipu-merchant/src/main/java/com/xiaozipu/merchant/web/OrderController.java
@@ -1,8 +1,12 @@
 package com.xiaozipu.merchant.web;
 
+import com.xiaozipu.common.result.ResultInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author: YinJunJie
@@ -13,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     private static final Logger logger= LoggerFactory.getLogger(OrderController.class);
 
+    @GetMapping
+    public ResultInfo orderList(HttpServletRequest request){
+        ResultInfo resultInfo= new ResultInfo();
+        return resultInfo;
+    }
 }
